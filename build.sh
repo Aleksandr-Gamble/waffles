8c5fcf1712ceb24d514876b61d23b6b99091463d
Aleksandr-Gamble <Aleksandr.Gamble@gmail.com>
2021-12-23 14:58:33 -0500

    added build.sh, push.sh, and demo-lambda-python

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script accepts both the name of a folder and a version number
+# Example usage: $ ./push.sh demo-lambda-python 1.0
+# NOTE: It is expected the ECR_URI has a format like this:
+# "1234567890.abc.ecr.us-east-1.amazonaws.com" etc.
+
+# The first argument should be an image: i.e. the name of a folder
+FOLDER="$1"
+IMAGE="$1"
+FILE="${FOLDER}/Dockerfile"
+if ! [ -f "$FILE" ]; then
+    echo "'$FILE' DOES NOT EXIST!"
+    echo "Enter the name of the image (folder) and version you want to use: for example:"
+    echo "./push.sh demo-lambda-python 1.0 # builds and pushes the demo-lambda-python:1.0 image"
+    exit 1
+fi
+
+# The second argument should be the image version
+VERSION="$2"
+if [ -z "$VERSION" ]; then
+    echo "NO IMAGE VERSION SPECIFIED!"
+    echo "Enter the name of the image (folder) and version you want to use: for example:"
+    echo "./push.sh demo-lambda-python 1.0 # builds and pushes the demo-lambda-python:1.0 image"
+    exit 1
+fi 
+VERSIONED_IMAGE="${IMAGE}:${VERSION}"
+
+
+# Build the image
+echo "bulding ${VERSIONED_IMAGE}..."
+sudo docker build -t $VERSIONED_IMAGE ${FOLDER}/ 
+
